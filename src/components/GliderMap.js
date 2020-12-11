931e6ede344fc58e9be6aed210244954e16bd004
David Lawson <david@threesig.com>
2020-12-10 21:48:34 -0700

    replace Marker with Glider Marker.  Map them out.

--- src/components/GliderMap.js
+++ src/components/GliderMap.js
@@ -1,21 +1,22 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
+import GliderMarker from "./GliderMarker";
 import { withGoogleMap, withScriptjs, GoogleMap, Marker, InfoWindow } from 'react-google-maps';
-
 const BELFAST_DEFAULT_LOCATION = {
   lat: 54.607868,
   lng: -5.926437
 }
 
-const GliderMap = withScriptjs(withGoogleMap((props) => {
+const GliderMap = withScriptjs(withGoogleMap(props => {
+  const {stops} = props;
   return (
     <GoogleMap
-      defaultZoom={8}
+      defaultZoom={15}
       defaultCenter={BELFAST_DEFAULT_LOCATION}
+      height={500}
     >
-      <Marker
-        position={BELFAST_DEFAULT_LOCATION}
-        label={'Hi!'}
-      />
+      {stops.map(stop => (
+        <GliderMarker key={stop.id} stopId={stop.id} position={stop.coords} label={stop.name} departures={stop.departures} />
+      ))}
     </GoogleMap>
   )
 }))
