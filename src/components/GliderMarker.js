7a5a58d3e8043f2ee750ce71f5f3974fe9671b74
David Lawson <david@threesig.com>
2020-12-10 21:47:59 -0700

    new component to house Marker and departure table

---
+++ src/components/GliderMarker.js
@@ -0,0 +1,43 @@
+import React, {useState} from "react";
+import {Marker} from "react-google-maps";
+import * as UI from "../questions/Question3UI";
+
+const GliderMarker = ({position, label, departures}) => {
+
+  const [showDepartures, setShowDepartures] = useState(false);
+
+  const handleMouseOver = () => setShowDepartures(true);
+  const handleMouseOut = () => setShowDepartures(false);
+
+  return (
+  <div>
+    <Marker {...{position, label, onMouseOver: handleMouseOver, onMouseOut: handleMouseOut}} />
+    {showDepartures && (
+      <div>
+        <h2>{label}</h2>
+        <UI.DepartureTable>
+          <thead>
+          <tr>
+            <th>From</th>
+            <th>To</th>
+            <th>Minutes Remaining</th>
+          </tr>
+          </thead>
+          <tbody>
+          {departures.map(({from, to, min_until}) => (
+            <tr>
+              <td>{from}</td>
+              <td>{to}</td>
+              <td>{min_until}</td>
+            </tr>
+          ))}
+          </tbody>
+        </UI.DepartureTable>
+      </div>
+    )}
+
+  </div>
+  )
+}
+
+export default GliderMarker;
